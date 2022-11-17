5aa8398c3d3caea0e3462cf29040f63905aab0fb
Roberto Carrillo <32170285+Robertcl795@users.noreply.github.com>
2022-11-17 03:40:08 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_Validador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Regex validatePassword = new Regex("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{5,}$");
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result;
+            if (String.IsNullOrEmpty(textBox1.Text)) 
+            { 
+                MessageBox.Show("La contraseña no puede estar vacía", "Error", buttons);
+            }
+            else if (!validatePassword.IsMatch(textBox1.Text)) 
+            {
+                MessageBox.Show("Contraseña no válida", "Error", buttons);
+                textBox1.Text = String.Empty;
+            } else 
+            {
+                result = MessageBox.Show("Contraseña validada", "Éxito", buttons);
+                if (result == DialogResult.OK)
+                {
+                    this.Close();
+                }
+            }
+        }
+    }
+}
